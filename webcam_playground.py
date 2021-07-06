87a2b458c3680c277e4504c60f68f599c92bec5f
Raz Fakterman <rf6699@gmail.com>
2021-07-06 13:00:23 +0300

    playground image capture

--- webcam_playground.py
+++ webcam_playground.py
@@ -1,10 +1,16 @@
 import cv2
 import os.path as osp
-
+import numpy as np
+from utils import load_cutout_to_contours_and_fill
 fps = 60
-save_path = r"C:\Code\Datathon\Playground"
-cap = cv2.VideoCapture(0)
 fnum = 1
+save_path = r"C:\Code\Datathon\Playground"
+
+cutout_path = r"cutouts\sample_cutout.png"
+fill, contours = load_cutout_to_contours_and_fill(cutout_path)
+
+cap = cv2.VideoCapture(0)
+
 # Check if the webcam is opened correctly
 if not cap.isOpened():
     raise IOError("Cannot open webcam")
@@ -12,13 +18,15 @@ if not cap.isOpened():
 cnt = 0
 while True:
     ret, frame = cap.read()
-    frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+    frame = cv2.resize(frame, None, fx=2, fy=2, interpolation=cv2.INTER_AREA)
+    frame = np.fliplr(frame)
+    frame = cv2.drawContours(frame, contours, -1, (255,255,255), 10)
     cv2.imshow('Input', frame)
 
     cnt+=1
     if cnt >= 120:
         cnt=0
-        cv2.imwrite(osp.join(save_path,f"{fnum}.png"), frame)
+        # cv2.imwrite(osp.join(save_path,f"{fnum}.png"), frame)
         fnum+=1
     c = cv2.waitKey(1)
     if c == 27:
